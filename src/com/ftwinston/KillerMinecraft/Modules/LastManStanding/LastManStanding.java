38dea3afbada590f8def68c2e37376a70b392744
FTWinston <ftwinston@gmail.com>
2015-11-24 13:33:07 +0000

    Added "shrinking borders" option, moved chest population code into SpawnPopulator class

--- src/com/ftwinston/KillerMinecraft/Modules/LastManStanding/LastManStanding.java
+++ src/com/ftwinston/KillerMinecraft/Modules/LastManStanding/LastManStanding.java
@@ -10,22 +10,14 @@ import org.bukkit.DyeColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.World;
 import org.bukkit.World.Environment;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.world.WorldInitEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionType;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
@@ -42,10 +34,10 @@ import com.ftwinston.KillerMinecraft.Configuration.ToggleOption;
 
 public class LastManStanding extends GameMode
 {
-	ToggleOption useTeams, centralizedSpawns;
+	ToggleOption useTeams, centralizedSpawns, shrinkingBorders;
 	NumericOption numTeams, numLives;
 	
-	static final long centralSpawnImmobilizationDelay = 200;
+	static final long centralSpawnImmobilizationDelay = 200, shrinkingBorderMoveInterval = 300;
 	long centralSpawnImmobilizationEnd;
 	
 	abstract class LMSTeamInfo extends TeamInfo
@@ -124,27 +116,37 @@ public class LastManStanding extends GameMode
 		useTeams = new ToggleOption("Use Teams", false, "Allows players to be divided", "into separate teams.") {
 			@Override
 			public void changed()
-			{
-				numTeams.setHidden(!isEnabled()); // can only set team count when teams enabled
-				
-				if ( isEnabled() )
+			{	
+				if (isEnabled())
 				{
-					// can only use centralized spawns when teams disabled
+					// when teams enabled, can choose number of teams, cannot use central spawns and shrinking borders
+					numTeams.setHidden(false);
 					centralizedSpawns.setHidden(true);
+					shrinkingBorders.setHidden(true);
 					
 					if ( centralizedSpawns.isEnabled() )
 						centralizedSpawns.toggle();
+					
+					if (shrinkingBorders.isEnabled())
+						shrinkingBorders.toggle();
+					
 					setTeams(teams);
 				}
 				else
 				{
+					// when teams disabled, cannot choose number of teams, can use central spawns and shrinking borders
+					numTeams.setHidden(true);
 					centralizedSpawns.setHidden(false);
+					shrinkingBorders.setHidden(false); // only show if the game is set to use world borders? no API access to that in advance
 					setTeams(new TeamInfo[0]);
 				}
 			}
 		};
 		
-		return new Option[] { useTeams, numTeams, centralizedSpawns, numLives };
+		shrinkingBorders = new ToggleOption("Shrinking Borders", false, ChatColor.RED + "Only if world borders are enabled", ChatColor.RESET + "When enabled, the world borders", "will get smaller every time", "a player is eliminated.");
+		shrinkingBorders.setHidden(true);
+		
+		return new Option[] { useTeams, numTeams, centralizedSpawns, shrinkingBorders, numLives };
 	}
 	
 	@Override
@@ -155,14 +157,23 @@ public class LastManStanding extends GameMode
 		if ( useTeams.isEnabled() )
 		{
 			messages.add("Players have been split into " + teams.length + " teams. Each time you die, your team's lives decreases by 1.");
-			messages.add("When a team has no lives left, they cannot respawn. The last team standing wins the game!");
-			messages.add("Your compass will point to the nearest player on another team.");
+			messages.add("When a team has no lives left, they cannot respawn, and will all die. The last team standing wins the game!");
+			messages.add("Compasses will point to the nearest player on another team.");
 		}
 		else
-		{
-			messages.add("Each time you die, your lives decreases by 1.");
-			messages.add("When you have no lives left, you cannot respawn. The last player standing wins the game!");
-			messages.add("Your compass will point to the nearest player apart from yourself.");
+		{	
+			if (numLives.getValue() > 1)
+			{
+				messages.add("Each time you die, your lives decreases by 1.");
+				messages.add("When you have no lives left, you cannot respawn. The last player standing wins the game!");
+			}
+			else 
+				messages.add("When you die, you will not respawn. The last player standing wins the game!");
+			
+			messages.add("Compasses will point to the nearest player apart from yourself.");
+			
+			if (shrinkingBorders.isEnabled() && !shrinkingBorders.isHidden())
+				messages.add("The world borders will get smaller every time a player is eliminated. Keep out of ther way!");
 		}
 		return messages;
 	}
@@ -311,14 +322,18 @@ public class LastManStanding extends GameMode
 		}
 	}
 
-	boolean inWarmup = true;
-	int nextPlayerNumber = 1;
+	boolean inWarmup = true, shouldShrinkWorldBorders;
+	int nextPlayerNumber = 1, initialNumPlayers, initialBorderRadius;
 	
 	@Override
 	public void gameStarted()
 	{
 		List<Player> players = getOnlinePlayers();
 
+		initialBorderRadius = (int)getWorld(0).getWorldBorder().getSize();
+		shouldShrinkWorldBorders = shrinkingBorders.isEnabled() && !shrinkingBorders.isHidden() && initialBorderRadius > 1;
+		initialNumPlayers = players.size();
+		
 		if ( !useTeams.isEnabled() )
 		{
 			angularSeparation = 2 * Math.PI / teams.length;
@@ -355,7 +370,7 @@ public class LastManStanding extends GameMode
 				for (Player player : players)
 					immobilizePlayer(player, (int)centralSpawnImmobilizationDelay);
 
-				createCentralSpawnItems();
+				SpawnPopulator.createCentralSpawnItems(getWorld(0));
 			}
 
 			nextPlayerNumber = 1; // ensure that the player placement logic starts over again
@@ -373,146 +388,6 @@ public class LastManStanding extends GameMode
 			SpawnPopulator populator = new SpawnPopulator(event.getWorld().getSpawnLocation(), (int)spawnCircleRadius + 2, 14, 10);
 			event.getWorld().getPopulators().add(populator);
 		}
-	}
-		
-	private void createCentralSpawnItems()
-	{
-		World world = getWorld(0);
-		Location center = world.getSpawnLocation();
-		
-		// the area around the central spawn items should be clear of trees etc
-		for (int x = center.getBlockX() - 2; x <= center.getBlockX() + 2; x++)
-		{
-			for (int z = center.getBlockZ() - 2; z <= center.getBlockZ() + 2; z++)
-			{
-				Block b = world.getHighestBlockAt(x, z);
-				while (!isGround(b.getType()))
-				{
-					b.setType(Material.AIR);
-					b = b.getRelative(BlockFace.DOWN);
-				}
-			}
-		}
-		
-		// create some central items (couple of chests, enchanting table
-		Block b = world.getHighestBlockAt(center.getBlockX(),  center.getBlockZ());
-		b.setType(Material.ENCHANTMENT_TABLE);
-		
-		b = world.getHighestBlockAt(center.getBlockX() + 1,  center.getBlockZ() + 1);
-		populateRandomChest(b);
-		
-		b = world.getHighestBlockAt(center.getBlockX() + 1,  center.getBlockZ() - 1);
-		populateRandomChest(b);
-		
-		b = world.getHighestBlockAt(center.getBlockX() - 1,  center.getBlockZ() + 1);
-		populateRandomChest(b);
-		
-		b = world.getHighestBlockAt(center.getBlockX() - 1,  center.getBlockZ() - 1);
-		populateRandomChest(b);
-	}
-
-	private boolean isGround(Material type)
-	{
-		switch (type)
-		{
-		case GRASS:
-		case DIRT:
-		case STONE:
-		case COBBLESTONE:
-		case SAND:
-		case GRAVEL:
-		case SANDSTONE:
-		case STATIONARY_WATER:
-		case STATIONARY_LAVA:
-			return true;
-		default:
-			return false;
-		}
-	}
-
-	private void populateRandomChest(Block b)
-	{
-		// TODO Auto-generated method stub
-		b.setType(Material.TRAPPED_CHEST);
-		
-		Inventory inv = ((Chest)b.getState()).getBlockInventory();
-		Random r = new Random();
-		
-		int numItems = r.nextInt(8) + 4;
-		for (int i=0; i<numItems; i++)
-			inv.setItem(r.nextInt(36), selectRandomItem(r));
-	}
-
-	private ItemStack selectRandomItem(Random r)
-	{
-		switch (r.nextInt(32))
-		{
-		case 0:
-		case 1:
-			return new ItemStack(Material.STONE_SWORD);
-		case 2:
-			return new ItemStack(Material.IRON_SWORD);
-		case 3:
-		case 4:
-			return new ItemStack(Material.BOW);
-		case 5:
-		case 6:
-			return new ItemStack(Material.ARROW, r.nextInt(8) + 2);
-		case 7:
-		case 8:
-		case 9:
-			return new ItemStack(Material.STONE_PICKAXE);
-		case 10:
-			return new ItemStack(Material.IRON_PICKAXE);
-		case 11:
-			return new ItemStack(Material.IRON_HELMET);
-		case 12:
-			return new ItemStack(Material.IRON_CHESTPLATE);
-		case 13:
-			return new ItemStack(Material.IRON_LEGGINGS);
-		case 14:
-			return new ItemStack(Material.IRON_BOOTS);
-		case 15:
-			return new ItemStack(Material.ENDER_PEARL, r.nextInt(3) + 1);
-		case 16:
-			return new ItemStack(Material.TNT, r.nextInt(4) + 1);
-		case 17:
-			return new ItemStack(Material.DIAMOND, 2);
-		case 18:
-			return new ItemStack(Material.BUCKET);
-		case 19:
-			return new ItemStack(Material.REDSTONE, r.nextInt(5) + 4);
-		case 20:
-			return new ItemStack(Material.COAL, r.nextInt(8) + 8);
-		case 21:
-			return createPotion(PotionType.SPEED, 1);
-		case 22:
-			return createPotion(PotionType.STRENGTH, 1);
-		case 23:
-			return createPotion(PotionType.INSTANT_HEAL, 2);
-		case 24:
-			return createPotion(PotionType.JUMP, 2);
-		case 25:
-			return createPotion(PotionType.NIGHT_VISION, 1);
-		case 26:
-			return createPotion(PotionType.REGEN, 1);
-		case 27:
-			return createPotion(PotionType.WATER_BREATHING, 1);
-		case 28:
-			return createPotion(PotionType.FIRE_RESISTANCE, 1);
-			
-		default:
-			return new ItemStack(Material.APPLE, r.nextInt(5) + 1);
-		}
-	}
-
-	private ItemStack createPotion(PotionType type, int level)
-	{
-		ItemStack stack = new ItemStack(Material.POTION);
-		Potion potion = new Potion(type);
-		potion.setLevel(level);
-		potion.apply(stack);
-		return stack;
 	}
 	
 	private void immobilizePlayer(Player player, int duration)
@@ -551,6 +426,8 @@ public class LastManStanding extends GameMode
 				// make every player on this team a spectator
 				for (Player player : getOnlinePlayers(new PlayerFilter().team(team)))
 					Helper.makeSpectator(getGame(), player);
+
+				checkForTeamVictory();				
 			}
 		}
 		else
@@ -559,55 +436,76 @@ public class LastManStanding extends GameMode
 			if ( score.getScore() > 0 )
 				score.setScore(score.getScore()-1);
 			else
+			{
 				Helper.makeSpectator(getGame(), event.getEntity());
+
+				checkForPlayerVictory();
+				
+				if (shouldShrinkWorldBorders && !hasGameFinished())
+					shrinkWorldBorders();
+			}
 		}
 	}
-	
+
 	@Override
 	public void playerQuit(OfflinePlayer player)
 	{
 		if ( hasGameFinished() )
 			return;
 		
-		if ( useTeams.isEnabled() )
-		{
-			// if only one team has players left, that team wins. Otherwise, continue.
-			
-			LMSTeamInfo lastTeam = null;
-			
-			for ( LMSTeamInfo team : teams )
-				if ( getOnlinePlayers(new PlayerFilter().team(team)).size() > 0 )
-				{
-					if ( lastTeam != null )
-						return; // multiple teams have players left
-					else
-						lastTeam = team;
-				}
-			
-			if ( lastTeam == null )
-				broadcastMessage("All players dead, game drawn");
-			else
-				broadcastMessage("The " + lastTeam.getChatColor() + lastTeam.getName() + ChatColor.RESET + " is the last team standing. The " + lastTeam.getName() + " wins!");
-				
-			finishGame();
-		}
+		if (useTeams.isEnabled())
+			checkForTeamVictory();
 		else
-		{
-			List<Player> survivors = getOnlinePlayers(new PlayerFilter());
-			
-			if ( survivors.size() == 1 )
+			checkForPlayerVictory();
+	}
+	
+	private void checkForTeamVictory()
+	{
+		// if only one team has players left, that team wins. Otherwise, continue.
+		
+		LMSTeamInfo lastTeam = null;
+		
+		for ( LMSTeamInfo team : teams )
+			if ( getOnlinePlayers(new PlayerFilter().team(team)).size() > 0 )
 			{
-				Player survivor = survivors.get(0);
-				broadcastMessage(new PlayerFilter().exclude(survivor), survivor.getName() + " is the last man standing, and wins the game!");
-				survivor.sendMessage("You are the last man standing: you win the game!");
+				if ( lastTeam != null )
+					return; // multiple teams have players left
+				else
+					lastTeam = team;
 			}
-			else if ( survivors.size() == 0 )
-				broadcastMessage("All players died, nobody wins!");
-			else
-				return; // multiple people left in the game
+		
+		if ( lastTeam == null )
+			broadcastMessage("All players dead, game drawn");
+		else
+			broadcastMessage("The " + lastTeam.getChatColor() + lastTeam.getName() + ChatColor.RESET + " is the last team standing. The " + lastTeam.getName() + " wins!");
 			
-			finishGame();
+		finishGame();
+	}
+	
+	private void checkForPlayerVictory()
+	{
+		List<Player> survivors = getOnlinePlayers(new PlayerFilter());
+		
+		if ( survivors.size() == 1 )
+		{
+			Player survivor = survivors.get(0);
+			broadcastMessage(new PlayerFilter().exclude(survivor), survivor.getName() + " is the last man standing, and wins the game!");
+			survivor.sendMessage("You are the last man standing: you win the game!");
 		}
+		else if ( survivors.size() == 0 )
+			broadcastMessage("All players died, nobody wins!");
+		else
+			return; // multiple people left in the game
+		
+		finishGame();
+	}
+	
+	private void shrinkWorldBorders()
+	{
+		double numPlayers = getOnlinePlayers(new PlayerFilter()).size();	
+		double targetRadius = initialBorderRadius * numPlayers / initialNumPlayers;  
+		
+		getWorld(0).getWorldBorder().setSize(targetRadius, shrinkingBorderMoveInterval);
 	}
 	
 	@Override
--- src/com/ftwinston/KillerMinecraft/Modules/LastManStanding/SpawnPopulator.java
+++ src/com/ftwinston/KillerMinecraft/Modules/LastManStanding/SpawnPopulator.java
@@ -7,7 +7,13 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Chest;
 import org.bukkit.generator.BlockPopulator;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionType;
 
 class SpawnPopulator extends BlockPopulator
 {
@@ -64,20 +70,147 @@ class SpawnPopulator extends BlockPopulator
 		
 	}
 	
-	private boolean isGround(Material type)
+	private static boolean isGround(Material type)
 	{
 		switch(type)
 		{
 		case GRASS:
 		case DIRT:
 		case STONE:
-		case SAND:
+		case COBBLESTONE:
 		case GRAVEL:
+		case SAND:
+		case SANDSTONE:
 		case BEDROCK:
 			return true;
 		
 		default:
 			return false;
 		}
+	}
+
+	public static void createCentralSpawnItems(World world)
+	{
+		Location center = world.getSpawnLocation();
+		
+		// the area around the central spawn items should be clear of trees etc
+		for (int x = center.getBlockX() - 2; x <= center.getBlockX() + 2; x++)
+		{
+			for (int z = center.getBlockZ() - 2; z <= center.getBlockZ() + 2; z++)
+			{
+				Block b = world.getHighestBlockAt(x, z);
+				while (!isGround(b.getType()))
+				{
+					b.setType(Material.AIR);
+					b = b.getRelative(BlockFace.DOWN);
+				}
+			}
+		}
+		
+		// create some central items (couple of chests, enchanting table
+		Block b = world.getHighestBlockAt(center.getBlockX(),  center.getBlockZ());
+		b.setType(Material.ENCHANTMENT_TABLE);
+		
+		b = world.getHighestBlockAt(center.getBlockX() + 1,  center.getBlockZ() + 1);
+		populateRandomChest(b);
+		
+		b = world.getHighestBlockAt(center.getBlockX() + 1,  center.getBlockZ() - 1);
+		populateRandomChest(b);
+		
+		b = world.getHighestBlockAt(center.getBlockX() - 1,  center.getBlockZ() + 1);
+		populateRandomChest(b);
+		
+		b = world.getHighestBlockAt(center.getBlockX() - 1,  center.getBlockZ() - 1);
+		populateRandomChest(b);
+	}
+	
+	private static void populateRandomChest(Block b)
+	{
+		// TODO Auto-generated method stub
+		b.setType(Material.TRAPPED_CHEST);
+		
+		Inventory inv = ((Chest)b.getState()).getBlockInventory();
+		Random r = new Random();
+		
+		int numItems = r.nextInt(8) + 4;
+		for (int i=0; i<numItems; i++)
+			inv.setItem(r.nextInt(36), selectRandomItem(r));
+	}
+
+	private static ItemStack selectRandomItem(Random r)
+	{
+		switch (r.nextInt(38))
+		{
+		case 0:
+		case 1:
+			return new ItemStack(Material.STONE_SWORD);
+		case 2:
+			return new ItemStack(Material.IRON_SWORD);
+		case 3:
+		case 4:
+			return new ItemStack(Material.BOW);
+		case 5:
+		case 6:
+			return new ItemStack(Material.ARROW, r.nextInt(8) + 2);
+		case 7:
+		case 8:
+		case 9:
+			return new ItemStack(Material.STONE_PICKAXE);
+		case 10:
+			return new ItemStack(Material.IRON_PICKAXE);
+		case 11:
+			return new ItemStack(Material.IRON_HELMET);
+		case 12:
+			return new ItemStack(Material.IRON_CHESTPLATE);
+		case 13:
+			return new ItemStack(Material.IRON_LEGGINGS);
+		case 14:
+			return new ItemStack(Material.IRON_BOOTS);
+		case 15:
+			return new ItemStack(Material.ENDER_PEARL, r.nextInt(3) + 1);
+		case 16:
+			return new ItemStack(Material.TNT, r.nextInt(4) + 1);
+		case 17:
+			return new ItemStack(Material.DIAMOND, 2);
+		case 18:
+			return new ItemStack(Material.BUCKET);
+		case 19:
+			return new ItemStack(Material.REDSTONE, r.nextInt(5) + 4);
+		case 20:
+			return new ItemStack(Material.COAL, r.nextInt(8) + 8);
+		case 21:
+			return createPotion(PotionType.SPEED, 1);
+		case 22:
+			return createPotion(PotionType.STRENGTH, 1);
+		case 23:
+			return createPotion(PotionType.INSTANT_HEAL, 2);
+		case 24:
+			return createPotion(PotionType.JUMP, 2);
+		case 25:
+			return createPotion(PotionType.NIGHT_VISION, 1);
+		case 26:
+			return createPotion(PotionType.REGEN, 1);
+		case 27:
+			return createPotion(PotionType.WATER_BREATHING, 1);
+		case 28:
+			return createPotion(PotionType.FIRE_RESISTANCE, 1);
+		case 29:
+		case 30:
+		case 31:
+		case 32:
+		case 33:
+			return new ItemStack(Material.COMPASS);
+		default:
+			return new ItemStack(Material.APPLE, r.nextInt(5) + 1);
+		}
+	}
+
+	private static ItemStack createPotion(PotionType type, int level)
+	{
+		ItemStack stack = new ItemStack(Material.POTION);
+		Potion potion = new Potion(type);
+		potion.setLevel(level);
+		potion.apply(stack);
+		return stack;
 	}
 }
